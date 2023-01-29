04ed5ec384b7572f3526c60be33847227e2c073e
Gerald.okeke <okekegerald@gmail.com>
2023-01-29 17:36:12 +0100

    added env var to travis file

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const favoriteFood = process.env.FAVORITE_FOOD
+const FAVORITE_INSTRUCTOR = process.env.NAME
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -6,7 +6,7 @@ function sleep(ms) {
 async function main() {
   while(true) {
     // console.log("Microservices rock!");
-    console.log(`The food we love most at home is ${favoriteFood}`);
+    console.log(`${FAVORITE_INSTRUCTOR} rules!!`);
     await sleep(5000);
   }
 }
