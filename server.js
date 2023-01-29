84839a375e73ad7cd1bf571bdb7f25c3ececd362
Gerald.okeke <okekegerald@gmail.com>
2023-01-29 17:03:47 +0100

    added travis file

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-
+const favoriteFood = process.env.FAVORITE_FOOD
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    // console.log("Microservices rock!");
+    console.log(`my favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
 }
